5337b0054e319fbd150a0374822617db3eb0e348
Amber van der Riet <56724503+110250-coder@users.noreply.github.com>
2022-09-28 08:05:46 +0000

    begin zelf code maken

--- main.py
+++ main.py
@@ -15,7 +15,64 @@ class MyAgent(MLAgent):
     
     
 random.seed(1)
- 
+
+def plot1():
+  class MyAgent(MLAgent):
+      def human(self, board):
+          if is_winner(board, self.symbol):
+              reward = 1
+          elif is_winner(board, opponent[self.symbol]):
+              reward = -1
+          else:
+              reward = 0
+          return reward
+def plot2():
+  class MyAgent(MLAgent):
+      def dumb(self, board):
+          if is_winner(board, self.symbol):
+              reward = 1
+          elif is_winner(board, opponent[self.symbol]):
+              reward = -1
+          else:
+              reward = 0
+          return reward
+def plot3():
+  class MyAgent(MLAgent):
+      def train(self, board):
+          if is_winner(board, self.symbol):
+              reward = 1
+          elif is_winner(board, opponent[self.symbol]):
+              reward = -1
+          else:
+              reward = 0
+          return reward
+def plot4():
+  class MyAgent(MLAgent):
+      def smart(self, board):
+          if is_winner(board, self.symbol):
+              reward = 1
+          elif is_winner(board, opponent[self.symbol]):
+              reward = -1
+          else:
+              reward = 0
+          return reward
+        
+def plot5():
+    class MyAgent(MLAgent):
+      def evaluate(self, board):
+          if is_winner(board, self.symbol):
+              reward = 1
+          elif is_winner(board, opponent[self.symbol]):
+              reward = -1
+          else:
+              reward = 0
+          return reward
+      
+    
+random.seed(1)
+
+plot_5 = plot5()
+
 my_agent = MyAgent()
 random_agent = RandomAgent()
  
